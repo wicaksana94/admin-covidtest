1a88e68e2be167ca1ae1ebea3fadef90d02ccac3
wicaksana94 <wicaksana94@gmail.com>
2020-06-30 20:13:49 +0700

    Init app

---
+++ src/app/shared/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render () {
+    return (
+      <footer className="footer">
+        <div className="container-fluid">
+          <div className="d-sm-flex justify-content-center justify-content-sm-between">
+            <span className="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Pointer © 2020</span>
+          </div>
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
