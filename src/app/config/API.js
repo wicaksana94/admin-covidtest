eba3f2e27d5ffe2065e515cf70e3c76dd3ae6c61
wicaksana94 <wicaksana94@gmail.com>
2020-07-27 13:28:22 +0700

    Fix req HTTP for admin and vendor

--- src/app/config/API.js
+++ src/app/config/API.js
@@ -1,5 +1,31 @@
 import axios from 'axios'
 
-export default axios.create({
+// Axios HTTP Request untuk data-data vendor
+let APIVendor = "http://localhost:2123/vendor/";
+
+// Axios HTTP Request untuk data-data admin
+let APIAdmin = "http://localhost:2123";
+
+// Axios HTTP Request untuk init login
+axios.create({
     baseURL:"http://localhost:2123"
 })
+
+axios.interceptors.request.use(
+    config => {
+        const idVendor = localStorage.getItem("vendorID");
+
+        // Sistem cek jika memiliki vendorID maka diidentifikasi pengguna vendor dan akan menggunakan APIVendor
+        if (idVendor) {
+            config.baseURL = APIVendor+idVendor;
+        } else {
+            config.baseURL = APIAdmin;
+        }
+
+        return config;
+    },
+
+    error => Promise.reject(error)
+);
+
+export default axios
