3657721805df9cf8c151238d399d967a8388ee99
wicaksana94 <wicaksana94@gmail.com>
2020-09-09 09:15:52 +0700

    Change logo to pointer icon in mobile layout

--- src/app/shared/Navbar.js
+++ src/app/shared/Navbar.js
@@ -9,7 +9,7 @@ class Navbar extends Component {
     return (
       <nav className="navbar col-lg-12 col-12 p-lg-0 fixed-top d-flex flex-row">
         <div className="navbar-menu-wrapper d-flex align-items-center justify-content-between">
-        <a className="navbar-brand brand-logo-mini align-self-center d-lg-none" href="!#" onClick={evt =>evt.preventDefault()}><img src={require("../../assets/images/logo-mini.svg")} alt="logo" /></a>
+        <a className="navbar-brand brand-logo-mini align-self-center d-lg-none" href="!#" onClick={evt =>evt.preventDefault()}><img src={require("../../assets/images/favicon.ico")} width={40} alt="logo" /></a>
           <button className="navbar-toggler navbar-toggler align-self-center" type="button" onClick={ () => document.body.classList.toggle('sidebar-icon-only') }>
             <i className="mdi mdi-menu"></i>
           </button>
